8ed8247363b36caacc0d2e8af19ea4815895e822
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2026-02-04 17:20:05 +0530

    Completed IIElevenLabs

--- assignment-2/easy/quiz-app/data.js
+++ assignment-2/easy/quiz-app/data.js
@@ -1,5 +1,5 @@
 //  use this quizData in your app.
-export const quizData = [{
+const quizData = [{
     "question": "Which language runs in a web browser?",
     "a": "Java",
     "b": "C",
@@ -33,3 +33,48 @@ export const quizData = [{
 },
 // you can add more quiz here
 ]
+
+let res = 0;
+
+document.getElementById('cards').style.display = 'none';
+
+function quiz(quizData) {
+    document.getElementById('question').textContent = quizData['question'];
+    document.getElementById("first").textContent = quizData['a'];
+    // document.getElementById("a").setAttribute("value", "a");
+    document.getElementById("second").textContent = quizData["b"];
+    // document.getElementById("b").setAttribute("value", "b");
+    document.getElementById("third").textContent = quizData["c"];
+    // document.getElementById("c").setAttribute("value", "c");
+    document.getElementById("fourth").textContent = quizData["d"];
+    // document.getElementById("d").setAttribute("value", "d");
+}
+
+let i;
+
+document.getElementById("start").addEventListener('click', start);
+document.getElementById("submit").addEventListener("click", () => onSubmit(i));
+
+function onSubmit(n) {
+    const selected = document.querySelector('input[name="opt"]:checked');
+    if (!selected) {
+        alert('please select a option before submitting');
+    } else {
+        if (selected.value == quizData[i]["correct"]) {
+          res++;
+        }
+    }
+    console.log(res);
+    i++;
+    quiz(quizData[i]);
+}
+
+function start(){
+    document.getElementById("cards").style.display = "block";
+    document.getElementById("start").style.display = "none";
+    i = 0;
+    quiz(quizData[i]);
+}
+
+// start()
+
